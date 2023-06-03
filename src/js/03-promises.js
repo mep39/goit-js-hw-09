9637386b4eb7e95dee119838f163ad3daf52ae00
mepinit <evgenmep@gmail.com>
2023-06-03 12:52:56 +0300

    3 task finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,51 +1,44 @@
-const submit = document.querySelector('.form');
+// Import library
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    resolve({ position, delay });
-  } else {
-    // Reject
-    reject({ position, delay });
-  }
-}, delay);
-});
-}
+// Get form element
+const formRef = document.querySelector('.form');
 
-function handlerSubmit(event) {
-  event.preventDefault();
+// Set event listener submit on form
+formRef.addEventListener('submit', onSubmitForm);
 
-  const inputAmount = document.querySelector('.promis-amount');
-  const inputAmountValue = parseInt(inputAmount.value);
+// Submit form
+function onSubmitForm(e) {
+  e.preventDefault();
 
-  const inputDelay = document.querySelector('.promis-delay');
-  const inputDelayValue = parseInt(inputDelay.value);
+  let delay = Number(formRef.delay.value);
 
-  const inputStep = document.querySelector('.promis-step');
-  const inputStepValue = parseInt(inputStep.value);
-
-  if (isNaN(inputAmountValue) || isNaN(inputDelayValue) || isNaN(inputStepValue)) {
-    alert('Please enter a valid number')
-    return
-  }
-  let currentDelay = inputDelayValue;
-  for (let i = 0; i < inputAmountValue; i++) {
-    const currentPosition = i + 1;
-    
-
-    createPromise(currentPosition, currentDelay)
+  for (let i = 1; i <= formRef.amount.value; i += 1) {
+    createPromise(i, delay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    
-    currentDelay += inputStepValue;
+    delay += Number(formRef.step.value);
   }
 }
 
-submit.addEventListener('submit', handlerSubmit);
+// Create promise
+function createPromise(position, delay) {
+  const obj = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(obj);
+      } else {
+        // Reject
+        reject(obj);
+      }
+    }, delay);
+  });
+}
